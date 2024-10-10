150cb3e2426431e16ccc859d0c4408e6143f9e13
Matthew0826 <DoubleEclipse8264@gmail.com>
2024-10-10 12:31:58 -0400

    Added education & work experience

--- militant-app/src/Resume.js
+++ militant-app/src/Resume.js
@@ -13,7 +13,8 @@ const Resume = () => {
     };
     return (
         <div>
-            <Parallax pages={3} ref={parallaxRef} >
+            <Parallax pages={4} ref={parallaxRef} >
+                {/*The Images*/}
                 < ParallaxLayer speed={0} offset={-.15}>
                     <img class={'main-content'} src='boston.jpeg'></img>
                     <div class='overlay'> </div>
@@ -23,7 +24,12 @@ const Resume = () => {
                     <img class={'main-content'} src='northeastern2.jpg'></img>
                     <div class='overlay'> </div>
                 </ParallaxLayer>
+                < ParallaxLayer speed={0} offset={1.5} >
+                    <img class={'main-content'} src='theengine.jpg'></img>
+                    <div class='overlay'> </div>
+                </ParallaxLayer>
 
+                {/*The Panels*/}
                 <ParallaxLayer speed={0.25} offset={0.12} style={{
                     fontSize: '5rem',
                     alignItems: 'center',
@@ -31,100 +37,149 @@ const Resume = () => {
                     textShadow: '2px 2px 4px rgba(0, 0, 0, 1)', // Text shadow for highlighting
                 }}> Overview</ParallaxLayer>
 
-                <ParallaxLayer speed={0.25} offset={.25} style={{
-                    height: '75vh',
-                    background: 'linear-gradient(to bottom , rgb(230, 230, 230), rgb(180, 180, 180))',
-                    boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
-                    display: 'flex',
-                }}>
-                    <div class="card" onClick={handlePress}>
-                        <h2>Education</h2>
-                        <img src="northeastern.jpg" class="cardIcon"></img>
-                        <h1>Northeastern<br />University</h1>
-                        <div class="cardText">
-                            <p>Mechanical & Computer <br />Engineering</p>
-                            <p>-- Class of 2026 --</p>
-                            <p>GPA: 3.947</p>
+                <ParallaxLayer speed={0.25} offset={.25}>
+                    <div class="panel">
+                        <div class="card" onClick={handlePress}>
+                            <h2>Education</h2>
+                            <img src="northeastern.jpg" class="cardIcon"></img>
+                            <h1>Northeastern<br />University</h1>
+                            <div class="cardText">
+                                <p>Mechanical & Computer <br />Engineering</p>
+                                <p>-- Class of 2026 --</p>
+                                <p>GPA: 3.947</p>
+                            </div>
+                        </div >
+                        <div class="card">
+                            <h2>Work Experience</h2>
+                            <img src="notch.jpeg" class="cardIcon"></img>
+                            <h1>Notch Inc. <br /><br /></h1>
+                            <div class="cardText">
+                                <p>Electrical & Computer<br />Engineering Co-Op</p>
+                                <p>January - July 2024</p>
+                                <p>RF Metamaterial Startup</p>
+                            </div>
                         </div>
-                    </div >
-                    <div class="card">
-                        <h2>Work Experience</h2>
-                        <img src="notch.jpeg" class="cardIcon"></img>
-                        <h1>Notch Inc. <br /><br /></h1>
-                        <div class="cardText">
-                            <p>Electrical & Computer<br />Engineering Co-Op</p>
-                            <p>January - July 2024</p>
-                            <p>RF Metamaterial Startup</p>
+                        <div class="card">
+                            <h2>Extracurricular Activities</h2>
+                            <img src="lunabotics.jpg" class="cardIcon"></img>
+                            <h1>NU Lunabotics<br /><br /></h1>
+                            <div class="cardText">
+                                <p>NASA Robotics <br />Competition</p>
+                                <p>Project Lead & Founder</p>
+                                <p>Team of 15+ People</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <h2>Extracurricular Activities</h2>
-                        <img src="lunabotics.jpg" class="cardIcon"></img>
-                        <h1>NU Lunabotics<br /><br /></h1>
-                        <div class="cardText">
-                            <p>NASA Robotics <br />Competition</p>
-                            <p>Project Lead & Founder</p>
-                            <p>Team of 15+ People</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <h2>Awards</h2>
-                        <img src="tbp.png" class="cardIcon"></img>
-                        <h1>Northeastern<br />Tau Beta Pi</h1>
-                        <div class="cardText">
-                            <p>For Academic Performance <br /> & Character</p>
-                            <p>Engineering Honor Society</p>
-                            <p>Member Since Spring 2024</p>
+                        <div class="card">
+                            <h2>Awards</h2>
+                            <img src="tbp.png" class="cardIcon"></img>
+                            <h1>Northeastern<br />Tau Beta Pi</h1>
+                            <div class="cardText">
+                                <p>For Academic Performance <br /> & Character</p>
+                                <p>Engineering Honor Society</p>
+                                <p>Member Since Spring 2024</p>
+                            </div>
                         </div>
                     </div>
                 </ParallaxLayer>
 
-                <ParallaxLayer speed={0.25} offset={1.1} style={{
-                    height: '75vh',
-                    backgroundColor: 'rgba(200, 200, 200, 1)',
-                    borderTop: '2px solid black',
-                    borderBottom: '2px solid black',
-                    display: 'flex',
-                }}>
-                    <div style={{
-                        width: '20%',
-                        border: '2px solid black', // Border for the left component
-                        borderRadius: '10px', // Rounded edges
-                        padding: '.1vw',
-                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', // Optional shadow for depth
-                        margin: '40px', // Space between the left component and main container
-                        backgroundColor: 'white',
-                        lineHeight: 1,
-                        fontSize: '100%',
-                    }}>
-                        <h1>Education</h1>
-                        <img src="northeastern.jpg" style={{
-                            width: '50%',
-                            borderRadius: '50%',
-                            aspectRatio: 1,
-                            objectFit: 'cover',
-                            border: '2px solid black', // Border for the left component
-                            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', // Optional shadow for depth
-                        }}></img>
-                        <h2>Northeastern University</h2>
-                        <h4>Mechanical & Computer Engineering</h4>
-                        <h4>-- Class of 2026 --</h4>
-                        <h4>GPA: 3.947</h4>
-                        <h5>CE Relevant Coursework:</h5>
-                        <p>Linear Systems, Engineering Algorithms, Electronics, Networks </p>
-                        <h5>MechE Relevant Coursework:</h5>
-                        <p>Fluid Mechanics, Dynamics, Compuation & Design</p>
-                        <button>More Education Experience</button>
+
+
+                <ParallaxLayer speed={0.25} offset={1.1}>
+                    <div class="panel">
+                        <div class="card edu" onClick={handlePress}>
+                            <h2>B.S. In Progress (NU)</h2>
+                            <img src="meche.jpg" class="cardIcon"></img>
+                            <h1>Mechanical<br />Engineering</h1>
+                            <div class="cardText">
+                                <p>-- Relevant Coursework --</p>
+                                <p><b>Current</b>: Comp. & Design</p>
+                                <p><b>Completed</b>: Fluids, Dynamics, Thermo, Material Sci., Mechanics of Materials</p>
+                            </div>
+                        </div >
+                        <div class="card edu">
+                            <h2>B.S. In Progress (NU)</h2>
+                            <img src="compe.jpg" class="cardIcon"></img>
+                            <h1>Computer<br />Engineering</h1>
+                            <div class="cardText">
+                                <p>-- Relevant Coursework --</p>
+                                <p><b>Current</b>: Linear Systems, Algorithms, Networks</p>
+                                <p><b>Completed</b>: Digital Design, Electronics</p>
+                            </div>
+                        </div>
+                        <div class="card edu">
+                            <h2>High School Diploma</h2>
+                            <img src="lrhs.jpg" class="cardIcon"></img>
+                            <h1>Lakeland Regional<br />High School</h1>
+                            <div class="cardText">
+                                <p>Wanaque, NJ</p>
+                                <p>-- Class of 2022 Salutatorian --</p>
+                                <p>Unweighted GPA: 4.0</p>
+                            </div>
+                        </div>
                     </div>
                 </ParallaxLayer>
 
-                <ParallaxLayer speed={0.25} offset={.99} style={{
+                <ParallaxLayer speed={0.25} offset={2}>
+                    <div class="panel">
+                        <div class="card" onClick={handlePress}>
+                            <h2>EECE CO-OP / Intern</h2>
+                            <img src="notch.jpeg" class="cardIcon"></img>
+                            <h1>Notch Inc.</h1>
+                            <div class="cardText">
+                                <p>Cambridge, MA</p>
+                                <p>January - July 2024< br />(part-time since)</p>
+                                <p><b>Skills:</b> PCB Design, Circuit Design, Python Programming, Rapid Prototyping, Full-Stack Development, RPi</p>
+                            </div>
+                        </div >
+                        <div class="card">
+                            <h2>EXPlorer</h2>
+                            <img src="makerspace.png" class="cardIcon"></img>
+                            <h1>EXP Makerspace</h1>
+                            <div class="cardText">
+                                <p>Boston, MA<br /> Northeastern University</p>
+                                <p>January 2023 - April 2024</p>
+                                <p><p><b>Skills:</b> Laser Cutting, 3D Printing, CAD, Product Design, First Aid Training, Customer Service</p></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <h2>Intern</h2>
+                            <img src="unilux.jpg" class="cardIcon"></img>
+                            <h1>Unilux Inc.</h1>
+                            <div class="cardText">
+                                <p>Saddle Brook, NJ</p>
+                                <p>June-August 2021 & 2022<br />May 2023</p>
+                                <p><b>Skills:</b> Soldering, Procedure Development, PID Controller, C++ Programming</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <h2>Adventure Guide</h2>
+                            <img src="flatwater.jpg" class="cardIcon"></img>
+                            <h1>Flatwater Paddle Co.</h1>
+                            <div class="cardText">
+                                <p>Ringwood, NJ</p>
+                                <p>August 2020, June-August<br />2021 & 2022, May 2023</p>
+                                <p><b>Skills:</b> Customer Service, Construction, Rescue Training, Communication</p>
+                            </div>
+                        </div>
+                    </div>
+                </ParallaxLayer>
+                {/* Text*/}
+
+                <ParallaxLayer speed={0.25} offset={.925} style={{
                     fontSize: '5rem',
                     alignItems: 'center',
                     size: '50vh',
                     color: "white",
                     textShadow: '2px 2px 4px rgba(0, 0, 0, 1)', // Text shadow for highlighting
                 }}><br /><br />Education</ParallaxLayer>
+
+                <ParallaxLayer speed={0.25} offset={1.825} style={{
+                    fontSize: '5rem',
+                    alignItems: 'center',
+                    size: '50vh',
+                    color: "white",
+                    textShadow: '2px 2px 4px rgba(0, 0, 0, 1)', // Text shadow for highlighting
+                }}><br /><br />Work Experience</ParallaxLayer>
 
 
             </Parallax>
